58ced2e61fc6dca8050a56fcf56dc31be9e0e370
Luiz-Eduardo-BL <limadudu34@gmail.com>
2023-05-10 18:07:29 -0300

    Create Project

---
+++ app/src/main/java/com/mobilegame/spacelunar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobilegame.spacelunar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
